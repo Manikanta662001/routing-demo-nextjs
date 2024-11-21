71a0d34b09ccc2b8040e5a831ca66f120b8f3494
Vamsi8074 <gundlurimanikanta142@gmail.com>
2024-11-21 18:13:10 +0530

    implemented parellel routes

---
+++ src/app/complex-dashboard/@users/page.tsx
@@ -0,0 +1,5 @@
+import Card from "@/components/Card";
+
+export default function UsersAnalytics() {
+  return <Card>This is Users Component</Card>;
+}
---
+++ src/app/complex-dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function ComplexDashboardPage() {
+  return <h2>This is Complex Dashboard Component</h2>;
+}
